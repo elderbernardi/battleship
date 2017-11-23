fdcfdab356c46831a0b993dfd3c13b9e4e5dcc83
Elder <elderbernardi@gmail.com>
2017-11-23 20:54:52 -0200

    versão com servidor

--- src/util/Estados.java
+++ src/util/Estados.java
@@ -1,21 +1,16 @@
+package util;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package servidortrabalho;
 
 /**
  *
  * @author elder
  */
-public class ServidorTrabalho {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public enum Estados {
+    CONECTADO, AUTENTICADO, SAIR
     
 }
