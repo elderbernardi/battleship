a6d128d3858b1c805629560e7a7875e706d70e72
Elder Bernardi <elderbernardi>
2017-11-23 17:24:04 -0200

    Versão somente com o jogo, sem server

---
+++ src/testes/Teste.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testes;
+
+import battleship.FilaJogadores;
+import battleship.Navio;
+import battleship.Tabuleiro;
+import battleship.TiroEnum;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author elder
+ */
+public class Teste {
+    
+    public static void main(String[] args) throws Exception {
+        Tabuleiro t = new Tabuleiro();
+        
+        System.out.println(t.desenhaTabuleiro());
+        
+        if(t.fimDeJogo())
+        {
+            //fim do jogo
+            t= new Tabuleiro();
+        }
+           
+     
+       
+       
+        for( int i=0; i<10; i++ )
+        {
+            for(int j=0; j<10;j++)
+            { 
+                t.atirar(i, j);
+                if(t.fimDeJogo()){
+                    System.out.println("FIM");
+                    j=10;
+                    i=10;
+                }
+            }
+        }
+                
+        System.out.println(t.desenhaTabuleiro() );
+      /*  for (Navio n : t.getNavios()) {
+            System.out.println(n.toString());
+            
+        }*/
+        
+        
+        
+    }
+    
+}
